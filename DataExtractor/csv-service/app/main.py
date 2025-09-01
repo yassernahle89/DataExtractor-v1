b4a56bf2a271d343b69b5cdb7e42b44fe7ffb2ce
yassernahle89 <157003034+yassernahle89@users.noreply.github.com>
2025-09-01 14:07:56 +0300

    add mongo writer into consumer

--- DataExtractor/csv-service/app/main.py
+++ DataExtractor/csv-service/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from shared_lib.kafka_consumer import KafkaConsumerService
+from shared_lib.mongo_connector import MongoWriter
 from dotenv import load_dotenv
 import os
 import json
@@ -7,7 +8,14 @@ import json
 app = FastAPI()
 
 load_dotenv()
-bootstrap_servers = os.getenv("BOOTSTRAP_SERVERS")
+bootstrap_servers = os.getenv("BOOTSTRAP_SERVERS","redpanda-0.uaenorth.cloudapp.azure.com:9094,redpanda-1.uaenorth.cloudapp.azure.com:9094,redpanda-2.uaenorth.cloudapp.azure.com:9094")
+mongodb_url=os.getenv("MONGODB_URL","mongodb+srv://yassern100:2XI4ryVCU2wcx81h@cluster0.hpzdmez.mongodb.net")
+
+
+# Globals
+mongo_writer: MongoWriter = None
+consumer: KafkaConsumerService = None
+
 
 # Common handler for both Kafka and API
 def print_if_json(msg):
@@ -26,9 +34,22 @@ def print_if_json(msg):
 # Handle Kafka messages
 def handle_kafka_message(msg: dict):
     print_if_json(msg)
+    if mongo_writer is not None:
+        mongo_writer.insert(msg)
 
 @app.on_event("startup")
 def start_kafka_consumer():
+    global mongo_writer, consumer
+
+    # setup Mongo
+    mongo_writer = MongoWriter(
+        uri=mongodb_url,
+        db="Redpanda",
+        collection="messages"
+    )
+    mongo_writer.connect()
+
+    # setup Kafka consumer
     consumer = KafkaConsumerService(
         topic="csv",
         bootstrap_servers=bootstrap_servers,
@@ -36,3 +57,15 @@ def start_kafka_consumer():
         callback=handle_kafka_message,
     )
     consumer.start()
+
+
+
+
+@app.on_event("shutdown")
+def stop_kafka_consumer():
+    global mongo_writer, consumer
+
+    if consumer:
+        consumer.stop()
+    if mongo_writer:
+        mongo_writer.close()
--- DataExtractor/word-service/app/main.py
+++ DataExtractor/word-service/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from shared_lib.kafka_consumer import KafkaConsumerService
+from shared_lib.mongo_connector import MongoWriter
 from dotenv import load_dotenv
 import os
 import json 
@@ -8,6 +9,11 @@ app = FastAPI()
 
 load_dotenv()
 bootstrap_servers = os.getenv("BOOTSTRAP_SERVERS")
+mongodb_url=os.getenv("MONGODB_URL")
+
+# Globals
+mongo_writer: MongoWriter = None
+consumer: KafkaConsumerService = None
 
 # Common handler for both Kafka and API
 def print_if_json(msg):
@@ -26,9 +32,21 @@ def print_if_json(msg):
 # Handle Kafka messages
 def handle_kafka_message(msg: dict):
     print_if_json(msg)
+    if mongo_writer is not None:
+        mongo_writer.insert(msg)
 
 @app.on_event("startup")
 def start_kafka_consumer():
+    global mongo_writer, consumer
+
+    # setup Mongo
+    mongo_writer = MongoWriter(
+        uri=mongodb_url,
+        db="Redpanda",
+        collection="messages"
+    )
+    mongo_writer.connect()
+
     consumer = KafkaConsumerService(
         topic="word",
         bootstrap_servers=bootstrap_servers,
@@ -36,3 +54,15 @@ def start_kafka_consumer():
         callback=handle_kafka_message,
     )
     consumer.start()
+
+
+
+@app.on_event("shutdown")
+def stop_kafka_consumer():
+    global mongo_writer, consumer
+
+    if consumer:
+        consumer.stop()
+    if mongo_writer:
+        mongo_writer.close()
+
